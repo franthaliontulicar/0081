8e840e6809a6a045ff8f9431d13845e3a14d14d1
paco <franmenendezpouso@gmail.com>
2016-03-07 11:35:42 +0100

    respuestas por palabras introducidas por el usuario

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -10,8 +11,9 @@ import java.util.Random;
 public class Responder
 {
     private Random random;
-   private ArrayList<String> respuestas;
-    
+    private ArrayList<String> respuestas;
+    private HashMap<String, String> palabrasClaves;
+
     /**
      * Construct a Responder - nothing to do
      */
@@ -24,14 +26,47 @@ public class Responder
         respuestas.add("El archivo es wimy timely");
         respuestas.add("El archivo no se puede leer");
         respuestas.add("Solo tengo 12 archivos tendran que valer para todos");
-    }
+        palabrasClaves = new HashMap<>();
+        String palabra = "hola";
+        String palabra2 = "cielito";
+        String palabra3 = "gas";
+        String palabra4 = "adios";
+        palabrasClaves.put(palabra,"hola que tal  estas?");
+        palabrasClaves.put(palabra2,"dime cielito");
+        palabrasClaves.put(palabra3,"dejastes cerrado el gas?");
+        palabrasClaves.put(palabra4,"adios!! nos vemos!!!");
 
+    }
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
     public String generateResponse()
     {
-         return  respuestas.get(random.nextInt(respuestas.size()));
+        return  respuestas.get(random.nextInt(respuestas.size()));
+    }
+
+    public String respuestaPorPalabra(String palabras){
+
+        String respuesta = "";
+
+        if(palabrasClaves.containsKey(palabras)){
+            respuesta = palabrasClaves.get(palabras);
+        }
+
+        else{
+            if(respuestas.size() > 0){
+                int num = random.nextInt(respuestas.size());
+                respuesta = respuestas.get(num);
+                respuestas.remove(respuesta);
+            
+            }
+            
+            else
+            {
+               respuesta = "No entiendo que me estas diciendo";
+            }
+        }
+        return respuesta;
     }
 }
--- SupportSystem.java
+++ SupportSystem.java
@@ -39,7 +39,7 @@ public class SupportSystem
         while(!finished) {
             String input = reader.getInput();
 
-            if(input.toLowerCase().contains("bye")) {
+            if(input.contains("bye")) {
                 finished = true;
             }
             else {
